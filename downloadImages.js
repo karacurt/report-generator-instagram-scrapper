1514390aaadf91261d7c8535eb494f6ff84de7db
Daniel Lima <daniel-lima@usp.br>
2021-12-21 16:47:31 -0300

    download image function separated

---
+++ downloadImages.js
@@ -0,0 +1,45 @@
+const fs = require("fs");
+const path = require("path");
+const request = require("request");
+
+const downloadImages = async (posts, from, until, to) => {
+  const postsDownloaded = [];
+  const untilDate = until ? new Date(until) : new Date();
+  const lastWeek = new Date(
+    untilDate.getFullYear(),
+    untilDate.getMonth(),
+    untilDate.getDate() - 7
+  );
+  const fromDate = from ? new Date(from) : lastWeek;
+  const toFolder = to ? to : "imgs";
+  if (!fs.existsSync(toFolder)) fs.mkdirSync(toFolder);
+
+  for (const post of posts) {
+    const fileName = post.img.split("/").pop().split("?")[0];
+    const pathToFile = path.resolve(`./${toFolder}/${fileName}`);
+    const isValid = await new Promise(function (resolve, reject) {
+      request.head(post.img, function (err, res, body) {
+        const postDate = new Date(res.headers["last-modified"]);
+        console.log("fileName", fileName);
+        console.log("from:", fromDate);
+        console.log("postDate:", postDate);
+        console.log("until:", untilDate);
+        console.log("isValid?:", postDate > fromDate && postDate < untilDate);
+        resolve(postDate > fromDate && postDate < untilDate);
+      });
+    });
+
+    if (!isValid) continue;
+
+    request(post.img).pipe(fs.createWriteStream(pathToFile));
+    console.log("The file was saved! ->", pathToFile);
+
+    postsDownloaded.push({
+      img: fileName,
+      description: post.description,
+    });
+  }
+  return postsDownloaded;
+};
+
+module.exports = { downloadImages };
--- makeppt.js
+++ makeppt.js
@@ -1,41 +1,7 @@
-const fs = require("fs");
-const path = require("path");
 const nodepptx = require("nodejs-pptx");
-const puppeteer = require("puppeteer");
-
 let pptx = new nodepptx.Composer();
-async function downloadImages(posts) {
-  const browser = await puppeteer.launch({
-    headless: false,
-  });
 
-  const postsDownloaded = [];
-  for await (const post of posts) {
-    const page = await browser.newPage();
-    await page.setViewport({ width: 1200, height: 1200 });
-    const fileName = post.img.split("/").pop().split("?")[0];
-    console.log(fileName);
-    var viewSource = await page.goto(post.img);
-    const pathToFile = await path.resolve(`./imgs/${fileName}`);
-    await fs.writeFile(pathToFile, await viewSource.buffer(), function (err) {
-      if (err) {
-        return console.log(err);
-      }
-    });
-    await page.waitFor(400);
-    console.log("The file was saved!");
-    postsDownloaded.push({ img: fileName, description: post.description });
-    await page.close();
-  }
-  await browser.close();
-
-  return postsDownloaded;
-}
-
-async function makeppt(posts) {
-  const postsDownloaded = await downloadImages(posts);
-  console.log(postsDownloaded);
-
+async function makeppt(postsDownloaded) {
   let count = 0;
   await pptx.compose(async (pres) => {
     console.log("entrou no compose");
@@ -74,4 +40,4 @@ async function makeppt(posts) {
   await pptx.save(`./report.pptx`);
 }
 
-module.exports = { makeppt, downloadImages };
+module.exports = { makeppt };
